658d8ecd8cfc87953765c69bd3381d2b04deedea
Adela SN <adela.nadhifa14@gmail.com>
2023-11-26 23:51:18 +0700

    add jokes crud and authorization

--- JokesWebApp/Data/Migrations/20231126151736_initialsetup.Designer.cs
+++ JokesWebApp/Data/Migrations/20231126151736_initialsetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JokesWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JokesWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231126151736_initialsetup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
---
+++ JokesWebApp/Views/Jokes/ShowSearchForm.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using JokesWebApp.Data;
+using JokesWebApp.Models;
+
+namespace JokesWebApp.Views.Jokes
+{
+    public class ShowSearchFormModel : PageModel
+    {
+        private readonly JokesWebApp.Data.ApplicationDbContext _context;
+
+        public ShowSearchFormModel(JokesWebApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Joke Joke { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Joke == null || Joke == null)
+            {
+                return Page();
+            }
+
+            _context.Joke.Add(Joke);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
